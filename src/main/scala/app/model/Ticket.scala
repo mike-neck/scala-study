5678040def88ea3d4f4d78c3fb2781a4bfc1e85c
mike_neck <jkrt3333@gmail.com>
2014-09-29 18:02:49 +0900

    IssueとBugを作成

--- src/main/scala/app/model/Ticket.scala
+++ src/main/scala/app/model/Ticket.scala
@@ -5,3 +5,5 @@ trait Ticket {
   val title: String
   val status: TicketStatus
 }
+case class Issue(id: Long, title: String, status: TicketStatus = TicketStatus.Open) extends Ticket
+case class Bug(id: Long, title: String, description: String, status: TicketStatus = TicketStatus.Open) extends Ticket
