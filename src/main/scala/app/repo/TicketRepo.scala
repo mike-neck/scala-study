9e8ed814e7b0de6b963c56d062114b297f37a881
mike_neck <jkrt3333@gmail.com>
2014-09-30 10:39:54 +0900

    TicketRepoを作成

---
+++ src/main/scala/app/repo/TicketRepo.scala
@@ -0,0 +1,13 @@
+package repo
+
+import model.TicketStatus.Fixed
+import model.{Bug, Issue, Ticket}
+
+object TicketRepo {
+  private var map: Map[Long, Ticket] = Map(
+    1L -> Issue(1, "メインページを作る", Fixed),
+    2L -> Issue(2, "サブページを作る"),
+    3L -> Bug(3, "メインページの左上のアイコンが違う", "こちらで指定したものを貼り付けてください"),
+    4L -> Bug(4, "Next Pageの綴りが違う", "Next PageとするべきところがNext Peijiになってるので直してください", Fixed)
+  )
+}
